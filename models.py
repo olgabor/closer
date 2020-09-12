2801106be7ed159de66e2d73dac3cbae135ece1e
Olb2 <olgaborysovska@gmail.com>
2020-09-11 19:02:37 -0700

    adds Project and Ticket models and  Status, Priority enums objects

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+import enum 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -32,8 +34,42 @@ class Project(db.Model):
     title = db.Column(db.String(150))
     description = db.Column(db.String(250)) 
     author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL')) #if user gets deleted this field gets deleted 
+    
+    #realtionship to Ticket 
+    tickets = relationship("Ticket", backref="project", cascade="all, delete")
+
 
     def __repr__(self):
             return f'Project(id={self.id}, title="{self.title}", description="{self.description}" , author_id="{self.author_id}")'
 
+
+class Status(enum.Enum):
+    'To Do' = 1
+    'In Progress' = 2
+    'Done'  = 3
+    'Cancelded' = 4 
+
+class Priority(enum.Enum):
+    'Higher' = 1
+    'High' = 2
+    'Medium'  = 3
+    'Low' = 4 
+
+class Ticket(db.Model): 
+    __tablename__ = 'ticket'
+
+    id = db.Column(db.Intger, primary_key=True)
+    summary = db.Column(db.Text) 
+    created_on = db.Column(db.Time, TIEMESTAMP, default=now, timezone=True) 
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='SET NULL')) #if user gets deleted this field gets deleted 
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id', ondelete='SET NULL')) #if project gets deleted this field gets deleted 
+    image = db.Column(db.String(300))
+    status = db.Enum(Status)
+    priority = db.Enum(Priority)
+    due_date = db.Column(db.Time, TIEMESTAMP, timezone=True) 
+
+    def __repr__(self):
+            return f'Ticket(id={self.id}, summary="{self.summary}", created_on="{self.created_on}" , author_id="{self.author_id}",  project_id ="{self.project_id}", status="{self.status}", priority="{self.priority}")'
+
   
+
