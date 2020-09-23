2213c6d61ec7fc4bf6c468cdbec9c2f5c71a2614
Olb2 <olgaborysovska@gmail.com>
2020-09-23 16:05:49 -0700

    updated postgresql url

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ login_manager.init_app(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://*/closer'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///closer'
 app.config['DEBUG'] = True
 
 # Sets the secret key to random bytes
