32e3a099d8092d5c8c301d43ac9edad40c58c373
Olb2 <olgaborysovska@gmail.com>
2020-10-28 09:55:30 -0700

    adds heroku database connection

--- models.py
+++ models.py
@@ -21,11 +21,11 @@ db = SQLAlchemy(app)
 # app.config['DEBUG'] = True
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-# DATABASE_URL = 'postgresql:///closer' 
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-# engine = create_engine('postgresql:///closer')
+os.environ['DATABASE_URL'] = 'postgres://jghziswjhhbsqc:bba0b2509ef51b1cc3d1939c3268ede973de62a63101d201545bb915094ce6a8@ec2-3-214-46-194.compute-1.amazonaws.com:5432/d5hhgj0jgibm7k'
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Sets the secret key to random bytes
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
