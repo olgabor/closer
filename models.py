33935aeb76c240b2f0d19e2d1722dcd1579998b3
Olb2 <olgaborysovska@gmail.com>
2020-10-28 14:52:09 -0700

    updated database url

--- models.py
+++ models.py
@@ -25,8 +25,8 @@ db = SQLAlchemy(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
-# os.environ['DATABASE_URL'] = 'postgres://jghziswjhhbsqc:bba0b2509ef51b1cc3d1939c3268ede973de62a63101d201545bb915094ce6a8@ec2-3-214-46-194.compute-1.amazonaws.com:5432/d5hhgj0jgibm7k'
-# DATABASE_URL = os.environ['DATABASE_URL']
+os.environ['DATABASE_URL'] = 'postgres://jghziswjhhbsqc:bba0b2509ef51b1cc3d1939c3268ede973de62a63101d201545bb915094ce6a8@ec2-3-214-46-194.compute-1.amazonaws.com:5432/d5hhgj0jgibm7k'
+DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
